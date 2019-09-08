3d535f1af22a0472951d04ed24177e19e48acb8a
Alyrace <alyson@pet-express.com>
2019-09-08 16:27:02 -0700

    skeleton added

---
+++ core/views.py
@@ -0,0 +1,14 @@
+from django.shortcuts import render
+from django.views.generic import TemplateView
+from .models import *
+
+class HomeTemplateView(TemplateView):
+    template_name = 'home.html'
+
+    # override get context date method
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)   # first, call super get context data
+        context['about'] = About.objects.first()
+        context['services'] = Services.objects.all()
+        context['work'] = RecentWork.objects.all()
+        return context
